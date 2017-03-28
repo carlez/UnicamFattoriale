1e54b56ecd4622bb3b2080f4285fecd880b2748e
Carlo Senzacqua <Carlo Senzacqua>
2017-03-28 09:39:59 +0200

    Controllo input utente

--- Program.cs
+++ Program.cs
@@ -6,18 +6,30 @@ namespace UnicamFattoriale
     {
         static void Main(string[] args)
         {
-            //Stampo dei messaggi di cortesia per l'utente, così lo guido all'utilizzo del programma
-            Console.WriteLine("Questo programma calcola il fattoriale di un numero intero");
-            Console.Write("Digita un numero intero e premi invio: ");
+            int numero = 0;
+            
+            while (true)
+            {
+                //Stampo dei messaggi di cortesia per l'utente, così lo guido all'utilizzo del programma
+                Console.WriteLine("Questo programma calcola il fattoriale di un numero intero");
+                Console.Write("Digita un numero intero e premi invio: ");
 
-            //Console.ReadLine si mette in attesa che l'utente digiti qualcosa e prema invio.
-            //Quando ha premuto invio, il testo che ha digitato lo assegno ad una variabile di tipo string.
-            string testoDigitatoDallUtente = Console.ReadLine();
+                //Console.ReadLine si mette in attesa che l'utente digiti qualcosa e prema invio.
+                //Quando ha premuto invio, il testo che ha digitato lo assegno ad una variabile di tipo string.
+                string testoDigitatoDallUtente = Console.ReadLine();
+                
+                try 
+                {
+                    //Interpreto il testo come un numero intero
+                    numero = int.Parse(testoDigitatoDallUtente);
+                    break;
+                } catch (Exception exc)
+                {
+                    Console.WriteLine("Attenzione, non hai inserito un numero intero!");
+                }
 
-            //Interpreto il testo come un numero intero
-            //Attenzione: questa istruzione fallirà se l'utente ha digitato un testo
-            //che non è interpretabile come un numero! (Es. ABCD)
-            int numero = int.Parse(testoDigitatoDallUtente);
+            }
+            
 
             //Calcolo il fattoriale: qui ci sono degli errori!
             int risultato = 0;
